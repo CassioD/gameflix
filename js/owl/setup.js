0f9e48c49aaeae733775f9fb968b794524a77e86
CassioD <cassiodans@gmail.com>
2021-08-01 23:54:05 -0300

    primeiro commit gameflix

---
+++ js/owl/setup.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+function clicar(){
+    window.location.href='https://youtu.be/tflKwXOESyc';
+}
+
+function informacoes(){
+    location.href='https://diablo4.blizzard.com/pt-br/';
+}
